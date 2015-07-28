84351c67741575f1a684181902d5dfe8241794d5
danbattista2 <danbattista2@outlook.com>
2015-07-27 20:48:43 -0700

    omg4

--- WebApplicationFinal/admin/adWorkout.aspx.designer.cs
+++ WebApplicationFinal/admin/adWorkout.aspx.designer.cs
@@ -76,6 +76,24 @@ namespace WebApplicationFinal.admin {
         protected global::System.Web.UI.WebControls.CompareValidator settsValidator;
         
         /// <summary>
+        /// txtWorkoutDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtWorkoutDate;
+        
+        /// <summary>
+        /// RangeValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
+        
+        /// <summary>
         /// btnSave control.
         /// </summary>
         /// <remarks>
